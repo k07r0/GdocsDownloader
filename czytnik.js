5053adb7c3f3bc9ece64596ee1e6535b241ebef7
Julitka <julitka@MacBook-Air-Macbook.local>
2023-11-27 11:12:07 +0100

    próby zainicjowania procesu ściągania ze strony google docs

--- czytnik.js
+++ czytnik.js
@@ -1,7 +1,6 @@
 let plik;
 let reader = new FileReader();
 
-
 /*document.getElementById("przycisk").onclick = function(){
     reader.readAsText(plik);
     window.alert(reader.result);
@@ -12,7 +11,8 @@ var changeIt = 'amp;';
 var putIt = '';
 const regexp = new RegExp('(?<=google[.]com).*?(?=show)','gs');
 const regAmp = new RegExp(changeIt, 'g');
-const regWhite = new RegExp(putIt,'g')
+const regWhite = new RegExp(putIt,'g');
+
 function konwerter (input) {
     plik = document.querySelector('#select-file').files[0];
     reader.readAsText(plik);
@@ -34,8 +34,9 @@ function display () {
             tablica[0][i] = pom;
             //console.log("https://docs.google.com" + tablica[0][i]);
             file = "https://docs.google.com" + tablica[0][i];
-           
-            downloadFile(file);
+            pobierz(file);
+            
+
             temp = '';
 
             i += 1;
@@ -54,7 +55,24 @@ function display () {
         }
     }
 
-    const downloadFile = (file) => {
+    async function pobierz (file) {
+            let blob = await fetch(file)
+            .then((response) => response.blob())
+            .then((blob) => {
+            // 2. Create blob link to download
+             const url = window.URL.createObjectURL(new File([blob], "obraz", {type: "image/png"}));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `sample.png`);
+             // 3. Append to html page
+             document.body.appendChild(link);
+             // 4. Force download
+             link.click();
+             // 5. Clean up and remove the link
+             link.parentNode.removeChild(link);
+            })}
+
+    /*const downloadFile = (file) => {
         const element = document.createElement('a');
         element.setAttribute('href', 'Download Btn', target="_blank");
         element.setAttribute('download', file);      
@@ -64,7 +82,7 @@ function display () {
       
         element.click();
         document.body.removeChild(element);
-      }
+      }*/
     
 
 
